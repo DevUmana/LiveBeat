07eb5341247b279b90ac69248e5be9c1938104ac
David <dumana92@gmail.com>
2024-10-05 20:31:23 -0400

    Added eventsData Interfac

--- client/src/interfaces/EventData.tsx
+++ client/src/interfaces/EventData.tsx
@@ -1,1 +1,8 @@
-// TODO
+export interface EventData {
+  title: string | null;
+  date: string | null;
+  time: string | null;
+  address: string | null;
+  link: string | null;
+  thumbnail: string | null;
+}

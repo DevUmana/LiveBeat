7c3e9bc4942d0c901c89d46c31ce76b155f58b1b
David <dumana92@gmail.com>
2024-10-07 21:05:52 -0400

    Added mock table

--- client/src/components/SearchEvent.tsx
+++ client/src/components/SearchEvent.tsx
@@ -1,13 +1,59 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import StoredEvents from "./StoredEvents";
+import Table from "./Table";
+
 const SearchEvent = () => {
+  const [search, setSearch] = useState<String | undefined>("");
+  const [result, setResult] = useState<Boolean | undefined>(false);
+  const [storedEvents, setStoredEvents] = useState<Boolean | undefined>(false);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (result === true) {
+      setResult(false);
+      setStoredEvents(false);
+    } else {
+      setResult(true);
+      setStoredEvents(true);
+    }
+  };
+
+  const handleTextChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    console.log(search);
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
   return (
     <>
-      <section className="search-event">
-        <form>
-          <label htmlFor="search">Search Event: </label>
-          <input type="text" id="search" name="search" />
-          <button type="submit">Search</button>
-        </form>
-        {/* Need to add result table */}
+      <section className="section-se">
+        <div className="container-se">
+          <div className="search-event">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="search">Search Event: </label>
+              <input
+                type="text"
+                id="search"
+                name="search"
+                onChange={handleTextChange}
+              />
+              <button type="submit" onSubmit={handleSubmit}>
+                Search
+              </button>
+            </form>
+          </div>
+          {!result ? (
+            <div>
+              <h2>Search for an event</h2>
+            </div>
+          ) : (
+            <Table />
+          )}
+        </div>
+        {!storedEvents ? null : <StoredEvents />}
       </section>
     </>
   );
---
+++ client/src/components/StoredEvents.tsx
@@ -0,0 +1,16 @@
+const StoredEvents = () => {
+  return (
+    <>
+      <aside className="aside-stored-event">
+        <h2>Stored Events</h2>
+        <ul>
+          <li>Event 1</li>
+          <li>Event 2</li>
+          <li>Event 3</li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default StoredEvents;

baa38d5f7b1dd828cfcd1c535aeff344f0eab576
David <dumana92@gmail.com>
2024-10-09 21:06:17 -0400

    Fixed bug

--- client/src/components/UpcomingEvents.tsx
+++ client/src/components/UpcomingEvents.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { retrieveUpcomingEvents } from "../api/ticketmasterAPI";
+import { EventData } from "../interfaces/EventData";
 import Card from "./Card";
 
 const UpcomingEvents = () => {
-  const [events, setEvents] = useState([]);
+  const [events, setEvents] = useState<EventData[]>([]);
 
   useEffect(() => {
     const fetchEvents = async () => {
